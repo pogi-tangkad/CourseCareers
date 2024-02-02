f8cabdb41dcbbffbe6df3aabdb41c99f69cd7c72
pogi-tangkad <pogi.tangkad@gmail.com>
2024-02-02 13:57:23 +0900

    reorganize files with new Lessons directory

--- Frontend_Basics/JavaScript/Mini-Projects/todo-list.js
+++ Frontend_Basics/JavaScript/Mini-Projects/todo-list.js
@@ -3,3 +3,17 @@
 */
 
 console.log("JavaScript ToDo List");
+
+function addTodo() {
+  const todo = document.getElementById("new-todo").value;
+  if (todo.length < 3) {
+    return alert("You must have at least 3 characters for your todo.");
+  }
+  const list = document.getElementById("list");
+  const li = document.createElement("li");
+  const text = document.createTextNode(todo);
+  li.setAttribute("ondblclick", "this.remove()");
+  li.appendChild(text);
+  list.appendChild(li);
+  document.getElementById("new-todo").value = "";
+}
