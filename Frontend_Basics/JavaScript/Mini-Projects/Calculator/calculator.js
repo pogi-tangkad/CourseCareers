91790269a87f1fb8de8cc5e3330173db99e0dd50
pogi-tangkad <pogi.tangkad@gmail.com>
2024-02-03 20:52:51 +0900

    Organized Mini Projects to separate folders
    completed calculator project:
    calculator.{html,css,js}
    
    ToDo-List,
     Random-Pwd-Generator,
     and Calculator folders

---
+++ Frontend_Basics/JavaScript/Mini-Projects/Calculator/calculator.js
@@ -0,0 +1,114 @@
+var calcDisplay = document.getElementById("calc-display");
+calcDisplay.style = "color: black";
+calcDisplay.style = "cell";
+let currentTotal = null;
+let nextValue = null;
+let pendingOperand = null;
+let currentOperand = null;
+let inputtingNumber = true;
+let finalOutput = false;
+let startDecimal = false;
+let decimalPlace = 1;
+
+function resetCalc() {
+  currentTotal = null;
+  nextValue = null;
+  pendingOperand = null;
+  currentOperand = null;
+  inputtingNumber = true;
+  finalOutput = false;
+  startDecimal = false;
+  decimalPlace = 1;
+}
+resetCalc();
+
+function updateCalc() {
+  if (currentOperand && currentTotal) {
+    console.log(currentOperand);
+    switch (currentOperand) {
+      case "*":
+        currentTotal *= nextValue;
+        break;
+      case "/":
+        currentTotal /= nextValue;
+        break;
+      case "+":
+        currentTotal += nextValue;
+        break;
+      case "-":
+        currentTotal -= nextValue;
+        break;
+    }
+    calcDisplay.innerText += "\n";
+    calcDisplay.innerText += "-------------------";
+    calcDisplay.innerText += "\n";
+    calcDisplay.innerText += currentTotal;
+    console.log(currentTotal);
+    calcDisplay.scrollTop = calcDisplay.scrollHeight;
+    currentOperand = null;
+  }
+}
+
+function numValue(num) {
+  if (finalOutput) {
+    resetCalc();
+    calcDisplay.innerText += "\n";
+    calcDisplay.innerText += "-------------------";
+    calcDisplay.innerText += "\n";
+    finalOutput = false;
+  }
+  if (!inputtingNumber) {
+    if (!currentTotal) {
+      currentTotal = nextValue;
+    }
+    currentOperand = pendingOperand;
+    pendingOperand = null;
+    nextValue = 0;
+    startDecimal = false;
+    decimalPlace = 1;
+    inputtingNumber = true;
+    calcDisplay.innerText += "\n";
+  }
+  if (startDecimal) {
+    decimalPlace *= 10;
+    nextValue = nextValue + num / decimalPlace;
+  } else {
+    nextValue = nextValue * 10 + num;
+  }
+  console.log(nextValue);
+  calcDisplay.innerText += num;
+  calcDisplay.scrollTop = calcDisplay.scrollHeight;
+}
+
+function operation(operand) {
+  updateCalc();
+  if (nextValue) {
+    if (pendingOperand) {
+      calcDisplay.innerText =
+        calcDisplay.innerText.substring(0, calcDisplay.innerText.length - 1) +
+        operand;
+    } else {
+      calcDisplay.innerText += "\n";
+      calcDisplay.innerText += operand;
+      calcDisplay.scrollTop = calcDisplay.scrollHeight;
+    }
+    pendingOperand = operand;
+    inputtingNumber = false;
+    finalOutput = false;
+  }
+}
+
+function calcEnter() {
+  updateCalc();
+  finalOutput = true;
+}
+
+function decimal() {
+  calcDisplay.innerText += ".";
+  startDecimal = true;
+}
+
+function clearDisplay() {
+  calcDisplay.innerText = "";
+  resetCalc();
+}
